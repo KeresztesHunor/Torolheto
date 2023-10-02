249c8f759739d14dc7fecb8ae8590091a7b22a8d
kereszteshunor <kereszteshunor36@gmail.com>
2023-10-02 08:53:18 +0200

    valami commit

--- src/mazdamiata/MazdaMiata.java
+++ src/mazdamiata/MazdaMiata.java
@@ -2,8 +2,33 @@ package mazdamiata;
 
 public class MazdaMiata
 {
+    private String[] pakli;
+    
     public static void main(String[] args)
     {
-        //this is a... mazda miata
+        new MazdaMiata().feladat();
+    }
+    
+    private void feladat()
+    {
+        pakli = new String[22];
+        String[] szinek = new String[] {
+            "S",
+            "H",
+            "C",
+            "D"
+        };
+        String[] szamok = new String[] {
+            "V",
+            "VI",
+            "VII",
+            "VIII",
+            "IX",
+            "X"
+        };
+        for (int i = 0; i < 10; i++)
+        {
+            
+        }
     }
 }
